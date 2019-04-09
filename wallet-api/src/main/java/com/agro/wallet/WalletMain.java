49661345879232cf49e1d61d72d5300e1c9b80e4
Sakhi Arya <sakhi.a@ezetap.com>
2019-04-09 13:22:24 +0530

    adding dependencies for modules and initial skelton

---
+++ wallet-api/src/main/java/com/agro/wallet/WalletMain.java
@@ -0,0 +1,25 @@
+package com.agro.wallet;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableTransactionManagement
+@EnableScheduling
+@ComponentScan({"com.agro"})
+@EntityScan("com.agro.dao")
+public class WalletMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletMain.class, args);
+    }
+
+
+}
